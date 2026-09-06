2ebc380f383e774a8da346963740465ee4301a8f
RiyazSyed28 <riyazsyed016@gmail.com>
2026-09-06 18:26:12 +0530

    Fix Aiven MySQL connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,21 +4,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT || 3306),
 
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
 
+    ssl: {
+        rejectUnauthorized: false,
+    },
+
     waitForConnections: true,
-
     connectionLimit: 10,
+    queueLimit: 0,
 
-    queueLimit: 0
-
+    connectTimeout: 20000,
 });
 
 export default pool;
